f5ba1740dbe44c7d66c760d8c922046137d2066c
v-wajian <v-wajian@andy30w11000>
2023-11-06 07:23:00 +0000

    Add project files.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using ClassLibrary1;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //instantiate the project
+            Person p = new Person();
+
+            label1.Text = p.Name;
+            label2.Text = p.Age.ToString();
+        }
+
+    }
+}
